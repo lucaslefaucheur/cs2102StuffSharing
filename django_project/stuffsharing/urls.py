8717d4180cb61f5ff0914f4ac03b82227c7177db
Lucas Le Faucheur <lucaslefaucheur@gmail.com>
2019-03-04 10:50:14 +0800

    add html files
    home + search + post + about

--- django_project/stuffsharing/urls.py
+++ django_project/stuffsharing/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='stuffsharing-home'),
+    path('search/', views.search, name='stuffsharing-search'),
+    path('post/', views.post, name='stuffsharing-post'),
     path('about/', views.about, name='stuffsharing-about'),
 ]
--- django_project/stuffsharing/views.py
+++ django_project/stuffsharing/views.py
@@ -5,6 +5,11 @@ from stuffsharing.models import TestUser, TestObject, IsWillingToRent
 def home(request):
     return render(request, 'stuffsharing/home.html')
 
+def search(request):
+    return render(request, 'stuffsharing/search.html')
+
+def post(request):
+    return render(request, 'stuffsharing/post.html')
 
 def about(request):
     return render(request, 'stuffsharing/about.html')
