9aecd1fcedae3c81545079f4b18544e49192655b
samad <samad.durussel@epfl.ch>
2019-04-09 15:36:24 +0200

    added all working form and pages for the MyAds pages, linked with curent DB

--- django_project/stuffsharing/forms.py
+++ django_project/stuffsharing/forms.py
@@ -18,3 +18,38 @@ class UserProfileInfoForm(forms.ModelForm):
 
 class SearchForm(forms.Form):
 	search=forms.CharField(max_length=30,widget=forms.TextInput(attrs={'class': "form-control form-control-lg form-control-borderless",'type':"Search",'placeholder':"Search stuffs or keywords"}))
+	
+class MyAdsAddForm(forms.Form):
+	tags = forms.CharField(label = 'Item tags:', max_length=1000,widget=forms.TextInput(attrs={'class': "form-control",'type':"Name",'placeholder':"tag1,tag2,..."}))
+	description = forms.CharField(label = 'Item Desctiption:', widget=forms.Textarea(attrs={'class': "form-control", 'rows':'3','type':"Description",'placeholder':"item description"}))
+	pic1 = forms.FileField(label = '', required=False)
+	pic2 = forms.FileField(label = '', required=False)
+	pic3 = forms.FileField(label = '', required=False)
+	
+class MyAdsInactiveForm(forms.Form):
+	stuff_for_lown=forms.IntegerField()
+	adName=forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': "form-control",'type':"Name",'placeholder':"Ad name"}), required=False)
+	start_date=forms.DateField(label = 'Pick up date:', required=False,widget=forms.widgets.SelectDateWidget())
+	end_date=forms.DateField(label = 'Return date:', required=False,widget=forms.widgets.SelectDateWidget())
+	price=forms.FloatField(label = 'Price', required=False)
+	pickupAddress=forms.CharField(label='Address',max_length=255, required=False)
+	returnAddress=forms.CharField(label='Address',max_length=255, required=False)
+	bid=forms.BooleanField(label='Bidding allowed',initial=False, required=False)
+	selectType=forms.ChoiceField(label='Selection', choices=[('auto','automatic'),('manual','manual')], widget=forms.RadioSelect,required=False)
+	submitter=forms.CharField()
+	
+	def clean(self):
+		cleaned_data = super().clean()
+		if cleaned_data['submitter']=='Post':
+			for field in cleaned_data:
+				if not field:
+					raise forms.ValidationError("All fields are required to create a new add")
+		return cleaned_data
+
+class MyAdsActiveBidForm(forms.Form):
+	loan_request_id=forms.IntegerField()
+	submitter=forms.CharField()
+
+class MyAdsActiveAdForm(forms.Form):
+	loan_prop_id=forms.IntegerField()
+	submitter=forms.CharField()
--- django_project/stuffsharing/views.py
+++ django_project/stuffsharing/views.py
@@ -3,6 +3,11 @@ from stuffsharing.models import  Profile, Stuff, LoanProposition, LoanRequest
 from .forms import SearchForm
 from .forms import SignUpForm
 from .forms import UserProfileInfoForm
+from .forms import MyAdsAddForm
+from .forms import MyAdsInactiveForm
+from .forms import MyAdsActiveBidForm
+from .forms import MyAdsActiveAdForm
+from django.db import connection
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -55,18 +60,96 @@ def search(request):
 
 
 def myadsadd(request):
-
-
-    return render(request, 'stuffsharing/myadsadd.html')
+	if request.user.is_authenticated :
+		if request.method=='POST':
+			form=MyAdsAddForm(request.POST)
+			if form.is_valid():
+				t=form.cleaned_data['tags']
+				d=form.cleaned_data['description']
+				o=request.user.profile
+				s=Stuff(tags=t,description=d,owner=o)
+				s.save()
+				
+	form=MyAdsAddForm()
+	return render(request, 'stuffsharing/myadsadd.html', {'form': form})
 
 def myadsactive(request):
-    
-	return render(request, 'stuffsharing/myadsactive.html')
-
-
+	if request.user.is_authenticated :
+		o=request.user.profile
+		if request.method=='POST':
+			if request.POST['submitter'] == 'remove':
+				form=MyAdsActiveAdForm(request.POST)
+				if form.is_valid():
+					p_id = form.cleaned_data['loan_prop_id']
+					with connection.cursor() as cursor:
+						cursor.execute("DELETE FROM stuffsharing_loanproposition WHERE id = %s",[p_id])
+					#LoanProposition.objects.raw("SELECT * FROM stuffsharing_loanproposition WHERE id = %s",[p_id]).delete()
+			else:
+				form=MyAdsActiveBidForm(request.POST)
+				if form.is_valid():
+					r_id = form.cleaned_data['loan_request_id']
+					if form.cleaned_data['submitter']=='accept':
+						lreq = LoanRequest.objects.raw('SELECT * FROM stuffsharing_loanrequest WHERE id = %s',[r_id])[0]
+						lreq.accepted=True
+						lreq.save()
+						lprop = lreq.original_proposition
+						lprop.available=False
+						lprop.save()
+						newloan = Loan(loan=lreq)
+						newloan.save()
+					else:
+						with connection.cursor() as cursor:
+							cursor.execute("DELETE FROM stuffsharing_loanrequest WHERE id = %s",[r_id])
+						#LoanRequest.objects.raw('SELECT * FROM stuffsharing_loanrequest WHERE id = %s',[r_id]).delete()
+		
+		propsList=LoanProposition.objects.raw("SELECT * FROM stuffsharing_loanproposition WHERE owner_id = %s",[o.user_id])
+		if len(propsList) > 0:
+			propsAndForms=[]
+			for prop in propsList:
+				lrequests=LoanRequest.objects.raw('SELECT * FROM stuffsharing_loanrequest WHERE original_proposition_id = %s',[prop.id])
+				reqAndForm=[]
+				for req in lrequests:
+					reqAndForm.append((req, MyAdsActiveBidForm(initial={'Loan_request_id': req.id})))
+				propsAndForms.append((prop, MyAdsActiveAdForm(initial={'loan_prop_id': prop.id}), reqAndForm))
+			return render(request, 'stuffsharing/myadsactive.html', {'propsAndForms': propsAndForms})
+		else:
+			return render(request, 'stuffsharing/myadsactive.html')
+	else:
+		return render(request, 'stuffsharing/myadsactive.html')
 
 def myadsinactive(request):
-    return render(request, 'stuffsharing/myadsinactive.html')
+	if request.user.is_authenticated :
+		o=request.user.profile
+		if request.method=='POST':
+			form=MyAdsInactiveForm(request.POST)
+			if form.is_valid():
+				if form.cleaned_data['submitter']=='Delete':
+					sid = form.cleaned_data['stuff_for_lown']
+					with connection.cursor() as cursor:
+							cursor.execute("DELETE FROM stuffsharing_stuff WHERE id = %s",[sid])
+					#Stuff.objects.raw('SELECt * FROM stuffsharing_stuff WHERE id = %s',[sid]).delete()
+				else:
+					sid = form.cleaned_data['stuff_for_lown']
+					aname = form.cleaned_data['adName']
+					sdate = form.cleaned_data['start_date']
+					edate = form.cleaned_data['end_date']
+					pr=form.cleaned_data['price']
+					paddr = form.cleaned_data['pickupAddress']
+					raddr = form.cleaned_data['returnAddress']
+					b=form.cleaned_data['bid']
+					stuff = Stuff.objects.raw('SELECT * FROM stuffsharing_stuff WHERE id = %s',[sid])[0]
+					newloanprop=LoanProposition(owner=o,name=aname,stuff_for_lown=stuff, start_date=sdate,end_date=edate,price=pr,pickupAdress=paddr,returnAdress=raddr,available=True,bid=b)
+					newloanprop.save()
+				
+		inactiveStuff = Stuff.objects.raw('SELECT * from stuffsharing_stuff S WHERE owner_id = %s AND NOT EXISTS(SELECT 1 FROM stuffsharing_LoanProposition WHERE stuff_for_lown_id = S.id)', [o.user_id])
+		if len(inactiveStuff)!=0:
+				propForms=[]
+				for item in inactiveStuff:
+					#propForms.append({'stuff': item, 'form': MyAdsInactiveForm(initial={'stuff_for_lown': item.id})})
+					propForms.append((item, MyAdsInactiveForm(initial={'stuff_for_lown': item.id,'pickupAddress': o.address, 'returnAddress': o.address})))
+				return render(request, 'stuffsharing/myadsinactive.html', {'formList': propForms})
+				
+	return render(request, 'stuffsharing/myadsinactive.html')
 
 def about(request):
     return render(request, 'stuffsharing/about.html')
